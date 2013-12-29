0f433d511d837fa68dd845d37fda0f3b36a5c61e
Thomas <mail@chabbster.de>
2013-12-30 00:46:03 +0100

    v0.1 Beta Release

---
+++ getstream.php
@@ -0,0 +1,11 @@
+<html>
+<meta http-equiv="refresh" content="8;url=test2.php">
+<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+<link href="css/flat-ui-new.css" rel="stylesheet">
+<?php
+$rpia = "127.0.0.1:8888";
+$npl = file_get_contents('http://' . ($rpia) . '/radio/nowplaying');
+$npla = json_decode($npl, true);
+echo '<button class="btn btn-inverse">Du h&ouml;rst</button> &nbsp;' . $npla['ResponseData']['Description'];
+?>
+</html>
